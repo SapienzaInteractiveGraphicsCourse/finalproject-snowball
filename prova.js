9cc6acd6a4946e9f2556eb3e104c19f3ec6b8875
flaviomax94 <falesiedi.1595746@studenti.uniroma1.it>
2019-06-18 17:41:42 +0200

    Add files via upload

--- prova.js
+++ prova.js
@@ -1,61 +1,42 @@
- window.addEventListener('DOMContentLoaded', function(){
-            // get the canvas DOM element
-            var canvas = document.getElementById('renderCanvas');
-
-            // load the 3D engine
-            var engine = new BABYLON.Engine(canvas, true);
+ 
+            var scene;
+            var canvas ;
+            var engine ;
 
             // createScene function that creates and return the scene
             var createScene = function(){
                 // create a basic BJS Scene object
-                var scene = new BABYLON.Scene(engine);
+                canvas= document.getElementById('renderCanvas');
+                engine= new BABYLON.Engine(canvas, true);
+                scene = new BABYLON.Scene(engine);
 
-                // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
                 var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
 
-                // target the camera to scene origin
+                
                 camera.setTarget(BABYLON.Vector3.Zero());
-
-                // attach the camera to the canvas
                 camera.attachControl(canvas, true);
 
                 // create a basic light, aiming 0,1,0 - meaning, to the sky
                 var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
-                // create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation 
-                //var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+                window.addEventListener('resize', function(){
+                engine.resize();
+                });
 
-                // move the sphere upward 1/2 of its height
-                //sphere.position.y = 1;
-
-                // create a built-in "ground" shape;
-                // The first parameter can be used to specify which mesh to import. Here we import all meshes
-                /*
                 var assetsManager = new BABYLON.AssetsManager(scene);
-
-                var meshTask = assetsManager.addMeshTask("ball task", "", "C:/Users/franco/Desktop/Interactive Graphics/project/ball-babylon/", "ball.babylon");
-
-                meshTask.onSuccess = function (task) {
-                    task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
-                }
-                meshTask.onError = function (task, message, exception) {
-                    console.log(message, exception);
-                }*/
-                var assetsManager = new BABYLON.AssetsManager(scene);
-                var meshCollisionerTask = assetsManager.addMeshTask("collisioners task", "", "/assets/", "ball.babylon");
+                var meshBallTask = assetsManager.addMeshTask("ball task", "", "assets/", "ball.babylon");
                 
-                meshCollisionerTask.onSuccess = function (task) {
-                var newMeshes = task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
-                /*newMeshes.forEach(function (element) {
-                     element.physicsImpostor = new BABYLON.PhysicsImpostor(element, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0 });
-                    element.visibility = false; //invisible block
-                });*/
+                meshBallTask.onSuccess = function (task) {
+                    task.loadedMeshes[0].position= BABYLON.Vector3.Zero();
                 };
                
-                meshCollisionerTask.onError = function (task, message, exception) {
+                meshBallTask.onError = function (task, message, exception) {
                     console.log(message, exception);
                 };
 
+                engine.runRenderLoop(function(){
+                scene.render();
+            });
                 assetsManager.load();
                 
                 //var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
@@ -64,16 +45,4 @@
                 return scene;
             };
 
-            // call the createScene function
-            var scene = createScene();
-
-            // run the render loop
-            engine.runRenderLoop(function(){
-                scene.render();
-            });
-
-            // the canvas/window resize event handler
-            window.addEventListener('resize', function(){
-                engine.resize();
-            });
-        });
+            
