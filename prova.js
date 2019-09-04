f4e8cbd0cd019cd5a8e3753ae55a097697e0ee3c
flaviomax94 <falesiedi.1595746@studenti.uniroma1.it>
2019-09-04 13:50:03 +0200

    Add files via upload

--- prova.js
+++ prova.js
@@ -146,7 +146,7 @@
 
                     var d1 = new BABYLON.DirectionalLight("dir", new BABYLON.Vector3(1, -1, 4), scene);
                     d1.position = new BABYLON.Vector3(-300,300,600);
-                    shadowGenerator = new BABYLON.ShadowGenerator(4096, d1);
+                    shadowGenerator = new BABYLON.ShadowGenerator(16382, d1);
                     shadowGenerator.transparencyShadow = true;
                     
 
@@ -399,7 +399,7 @@
         var mesh = newMeshes[0];
         mesh.position= new BABYLON.Vector3(13,0,-7);
         mesh.scaling = new BABYLON.Vector3(0.3, 0.3, 0.3);
-        shadowGenerator.getShadowMap().renderList.push(mesh);
+        //shadowGenerator.getShadowMap().renderList.push(mesh);
         for (var j = 0; j < 2; j++) {         
             
                 var clone = mesh.clone("newname");
